e857ff0124c0f9075c443a0b3a272f9039f79dcb
MoralesWJosue98 <00025719@uca.edu.sv>
2020-05-26 01:10:04 -0600

    PRE PARCIAL

---
+++ PreParcial/PREPARCIAL2/MODELO/InventoryDAO.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using PREPARCIAL2.CONTROLADOR;
+using PREPARCIAL2.MODELO;
+
+
+namespace PREPARCIAL2
+{
+    public static class InventoryDAO
+    {
+        public static List<Inventory> getLista()
+        {
+            string query = "SELECT * FROM INVENTORY";
+
+            DataTable dt = ConectionDB.ExecuteQuery(query);
+
+            List<Inventory> list = new List<Inventory>();
+            foreach (DataRow row in dt.Rows)
+            {
+                Inventory i = new Inventory();
+                i.product_name = row[0].ToString();
+                i.description = row[1].ToString();
+                i.price = Convert.ToDouble(row[2].ToString());
+                i.stock = Convert.ToInt32(row[3].ToString());
+
+                list.Add(i);
+            }
+
+            return list;
+        }
+       
+
+        public static void CreateProduct(string name, string description, double price, int stock)
+        {
+            string query = String.Format(
+                "INSERT INTO INVENTORY(product_name,description,price,stock) " + 
+                "VALUES('{0}','{1}','{2}','{3}')", name, description, price, stock
+                );
+            
+            ConectionDB.ExecuteNonQuery(query);    
+        }
+
+        public static void ModifyStock(int stock, string name)
+        {
+            string query = String.Format(
+                "UPDATE INVENTORY SET stock ={0} WHERE product_name ='{1}'", stock, name
+                );
+            ConectionDB.ExecuteNonQuery(query);
+        }
+        
+        public static void Delete(string name)
+        {
+            string query = String.Format(
+                "DELETE FROM INVENTORY WHERE product_name='{0}';", name
+            );
+                    
+            ConectionDB.ExecuteNonQuery(query);        
+        }
+
+       
+      
+        
+        
+        
+
+    }
+}
---
+++ PreParcial/PREPARCIAL2/MODELO/OrderDAO.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using PREPARCIAL2.CONTROLADOR;
+using PREPARCIAL2.MODELO;
+
+namespace PREPARCIAL2
+{
+    public class OrderDAO
+    {
+        public static List<Orders> getLista2()
+        {
+            string query = "SELECT * FROM ORDERS";
+
+            DataTable dt = ConectionDB.ExecuteQuery(query);
+            
+            List<Orders> list2 = new List<Orders>();
+            foreach (DataRow row in dt.Rows)
+            {
+                Orders o = new Orders();
+
+                o.id_order = Convert.ToInt32(row[0].ToString());
+                o.user_name = row[1].ToString();
+                o.product_name = row[2].ToString();
+                o.quantity = Convert.ToInt32(row[3].ToString());
+                o.id_user = Convert.ToInt32(row[4].ToString());
+                
+                list2.Add(o);
+            }
+            return list2;
+        }
+
+        public static void Buy(string name, string product, int quantity, int id)
+        {
+            string query = String.Format(
+                "INSERT INTO ORDERS(user_name,product_name,quantity,id_user) " +
+                "VALUES('{0}','{1}', '{2}','{3}' )", name, product, quantity,id
+                );
+            
+            ConectionDB.ExecuteNonQuery(query);
+        }
+        
+        
+        
+        public static List<Orders> getLista(string name)
+        {
+            string query = String.Format(
+                "SELECT * FROM ORDERS WHERE user_name='{0}';", name
+            );
+
+            DataTable dt = ConectionDB.ExecuteQuery(query);
+            
+            List<Orders> list = new List<Orders>();
+            foreach (DataRow row in dt.Rows)
+            {
+                Orders o = new Orders();
+
+                o.id_order = Convert.ToInt32(row[0].ToString());
+                o.user_name = row[1].ToString();
+                o.product_name = row[2].ToString();
+                o.quantity = Convert.ToInt32(row[3].ToString());
+                o.id_user = Convert.ToInt32(row[4].ToString());
+                
+                list.Add(o);
+            }
+            return list;
+        }
+        
+       
+        
+        
+        
+    }
+}
